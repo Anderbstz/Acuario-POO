1124bca36f009cf98a4b20dbc804b5a98d00ab3d
Anderbstz <anderbstz@gmail.com>
2025-09-04 22:03:30 -0500

    Nueva clase PecesAcuario

--- src/acuario/Main.kt
+++ src/acuario/Main.kt
@@ -1,12 +1,13 @@
 package acuario
 
-fun construirAcuario() {
-    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
-    miAcuario.imprimirTamano()
-    val miTorre = TanqueTorre(diametro = 25, alto = 40)
-    miTorre.imprimirTamano()
+fun crearPeces() {
+    val tiburon = Tiburon()
+    val pezPayaso = PezPayaso()
+
+    println("El tiburón es de color ${tiburon.color}")
+    println("El pez payaso es de color ${pezPayaso.color}")
 }
 
 fun main() {
-    construirAcuario()
+    crearPeces()
 }
